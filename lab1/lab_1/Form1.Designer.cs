62736295aee4d3edb6c7c2a3a56f4f5d429314e8
marcii16 <mrcllncn61@gmail.com>
2025-05-13 11:09:38 +0800

    finished lab_1

---
+++ lab1/lab_1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace lab1
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxResults = new ListBox();
+            btnSort = new Button();
+            SuspendLayout();
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.Location = new Point(264, 49);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(486, 284);
+            listBoxResults.TabIndex = 0;
+            // 
+            // btnSort
+            // 
+            btnSort.Location = new Point(23, 49);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(220, 56);
+            btnSort.TabIndex = 1;
+            btnSort.Text = "Sort";
+            btnSort.UseVisualStyleBackColor = true;
+            btnSort.Click += btnSort_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(816, 394);
+            Controls.Add(btnSort);
+            Controls.Add(listBoxResults);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBoxResults;
+        private Button btnSort;
+    }
+}
